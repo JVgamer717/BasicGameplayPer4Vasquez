2528d5a82e2f4037c17c4e3ae1557ed721aa6d47
sean <709475@student.dusd.net>
2025-04-08 11:47:52 -0700

    r
    r

--- BasicGameplayVasquezPer4/Assets/Course Library/Scripts/DestroyOutOfBounds.cs
+++ BasicGameplayVasquezPer4/Assets/Course Library/Scripts/DestroyOutOfBounds.cs
@@ -27,17 +27,17 @@ public class DestroyOutOfBounds : MonoBehaviour
         }
         else if(transform.position.z < lowerBound)
         {
-            Debug.Log("Game Over!");
+            gameManager.AddLives(-1);
             Destroy(gameObject);
         }
         else if (transform.position.x > sideBound)
         {
-            Debug.Log("Game Over!");
+            gameManager.AddLives(-1);
             Destroy(gameObject);
         }
         else if (transform.position.x < -sideBound)
         {
-            Debug.Log("Game Over!"); 
+            gameManager.AddLives(-1);
             Destroy(gameObject);
 
         }
